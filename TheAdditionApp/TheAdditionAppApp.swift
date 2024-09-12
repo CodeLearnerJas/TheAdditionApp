988dcfdf44d87466291ffe74a944051b575e7b2b
CodeLearnerJas <yangtianjin2013@gmail.com>
2024-09-12 13:17:39 +1000

    Initial Commit

---
+++ TheAdditionApp/TheAdditionAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheAdditionAppApp.swift
+//  TheAdditionApp
+//
+//  Created by GuitarLearnerJas on 12/9/2024.
+//
+
+import SwiftUI
+
+@main
+struct TheAdditionAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
